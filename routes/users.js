a006ad9b602b5b81173002c83df9c0067fc5419d
Yiyang Chiang <chiangyiyang@gmail.com>
2021-04-20 12:50:55 +0800

    1. 全部users的Json

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+//模擬的資料
+const users = [
+  { id: 'a1234', name: 'John' },
+  { id: 'a1235', name: 'Mary' },
+  { id: 'a1236', name: 'Tom' },
+]
+
+//全部users的Json
+router.get('/', function (req, res, next) {
+  res.json(users);
 });
 
 module.exports = router;
