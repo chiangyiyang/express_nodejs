e61976f8ca501212b9ebce10b7f28e628d2e21d3
Yiyang Chiang <chiangyiyang@gmail.com>
2021-04-20 14:01:32 +0800

    4. 更新user

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 //模擬的資料
-const users = [
-  { id: 'a1234', name: 'John' },
-  { id: 'a1235', name: 'Mary' },
-  { id: 'a1236', name: 'Tom' },
+let users = [
+  { id: 'a1234', name: 'John', email: 'john@gmail.com' },
+  { id: 'a1235', name: 'Mary', email: 'mary@gmail.com' },
+  { id: 'a1236', name: 'Tom', email: 'tom@gmail.com' },
 ]
 
 //全部users的Json
@@ -24,6 +24,31 @@ router.get('/:id', function (req, res, next) {
 //ex. curl -X POST -H 'Content-Type:application/json' -d '{"id":"a001","name":"Yiyang"}' http://localhost:3000/users
 router.post('/', function (req, res, next) {
   users.push(req.body);
+  res.json(users);
+});
+
+//更新user
+//ex. curl -X PUT -H 'Content-Type:application/json' -d '{"name":"John Doe", "email":"john@outlook.com"}' http://localhost:3000/users/a1234
+router.put('/:id', function (req, res, next) {
+  users = users.map(user => {
+    if (user.id == req.params.id) {
+      user = req.body;
+      user.id = req.params.id;
+    }
+    return user;
+  });
+  res.json(users);
+});
+
+//更新user部分資料
+//ex. curl -X PATCH -H 'Content-Type:application/json' -d '{"name":"John Doe"}' http://localhost:3000/users/a1234
+router.patch('/:id', function (req, res, next) {
+  users = users.map(user => {
+    if (user.id == req.params.id) {
+      Object.keys(req.body).forEach(key => user[key] = req.body[key])
+    }
+    return user;
+  });
   res.json(users);
 });
 
